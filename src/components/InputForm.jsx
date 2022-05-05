6797f4f9062d4ca39d879e6d1a686265fe25da6c
Chimoha31 <mokochii1108@gmail.com>
2022-05-04 21:33:27 -0700

    Done

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -4,7 +4,7 @@ import InputColor from "./InputColor";
 import InputField from "./InputField";
 
 const InputForm = () => {
-  const {getQrCode} = useContext(InputContext);
+  const {getQrCode, inputValue} = useContext(InputContext);
 
   const handleClick = () => {
     getQrCode();
@@ -17,6 +17,7 @@ const InputForm = () => {
       <button
         className="bg-blue-300 max-w-xs ml-auto px-4 py-2 text-white rounded-sm mt-4 hover:bg-blue-400 disabled:bg-gray-300"
         onClick={handleClick}
+        disabled={!inputValue.url}
       >
         Generate QrCode
       </button>
--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -1,39 +1,41 @@
 import React, { useContext } from "react";
 import { InputContext } from "./context/Context";
+import { saveAs } from "file-saver";
 
 const QrCode = () => {
   const { response, error, loading } = useContext(InputContext);
+  const handleDownloadImage = () => {
+    saveAs(response, "qrCode.png");
+  };
 
   if(loading) {
-    return(
+    return (
       <div className="animate-pulse flex flex-col items-center justify-center px-10 gap-3">
-        <div className="h-32 2-full bg-gray-300"></div>
-        <div className="h-8 2-full bg-gray-300"></div>
+        <div className="h-32 w-full bg-gray-300"></div>
+        <div className="h-8 w-full bg-gray-300"></div>
       </div>
     );
   }
 
   if(error) {
-    return <div className="text-gray-500 flex items-center">
-      Sorry, something went wrong. Try again!
-    </div>
+    return <div className="text-gray-500 flex items-center">Sorry, Something went wrong. Try again !</div>
   }
 
   return (
-    <div className="bg-gray-100 rounded-r-md flex flex-col items-center justify-center">
-        {response ? (  <div>
-        <img
-          className="w-48"
-          src={response}
-          alt="qrCode"
-        />
-        <button className="bg-blue-300 text-white mt-2 px-4 py-1 w-full">
-          Download
-        </button>
-      </div>) : (
+    <div className="bg-gray-100 rounded-r-md flex flex-col items-center justify-center py-4">
+      {response ? (
+        <div>
+          <img className="w-48" src={response} alt="qrCode" />
+          <button
+            onClick={handleDownloadImage}
+            className="bg-blue-400 text-white mt-2 px-4 py-1 w-full"
+          >
+            Download
+          </button>
+        </div>
+      ) : (
         <div className="text-gray-500">Your QrCode will showing here...</div>
       )}
-    
     </div>
   );
 };
